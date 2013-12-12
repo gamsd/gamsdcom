b001626e59a15d31273756b74d4e29e0d1bda308
Guilherme A. Dantas <gamsd@gamsd.com>
2013-12-12 20:32:10 -0300

    still trying to set favicon.ico

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ var fs = require('fs');
 var index = fs.readFileSync('index.html');
 
 http.createServer(function (req, res) {
+
+  if (q.url === '/favicon.ico') {
+    var img = fs.readFileSync('./favicon.ico');
+    res.writeHead(200, {"Content-Type": "image/x-icon"});
+    res.end(img,'binary');
+    return;
+  }
+
   res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
   res.end(index);
 }).listen(8080);
